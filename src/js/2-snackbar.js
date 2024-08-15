eecee056b3b518bb3435a375622b6ac308215790
Eugene Kuz <tir26@ukr.net>
2024-08-15 09:42:18 +0300

    remove unnecessery code

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -67,22 +67,3 @@ const createPromiseNotification = event => {
 
 submitForm.addEventListener('submit', createPromiseNotification);
 
-const options = {
-  method: 'GET',
-};
-
-fetch('https://jsonplaceholder.typicode.com/users', options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Data handling
-    console.log(data);
-  })
-  .catch(error => {
-    // Error handling
-    console.log(error);
-  });
